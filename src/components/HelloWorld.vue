8a3cdebe423493ca2f3cb9403650811c6ae5a162
Rodrigo <rdrgcornejo@hotmail.com>
2022-11-07 16:13:28 -0300

    inicia, zoom

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,44 @@
 <template>
-  <canvas ref="can" width="400" height="400"></canvas>
+  <canvas ref="can" width="600" height="600"></canvas>
 </template>
 
 <script>
 import { fabric } from 'fabric';
 
 export default {
+  data(){
+    return {
+      canvas: null
+    }
+  },
   mounted() {
     const ref = this.$refs.can;
-    const canvas = new fabric.Canvas(ref);
-    const rect = new fabric.Rect({
-      fill: 'red',
-      width: 20,
-      height: 20
+    this.canvas = new fabric.Canvas(ref);
+    this.canvas.on('mouse:wheel', function(opt) {
+      console.log("werqwer")
+      var delta = opt.e.deltaY;
+      var zoom = this.canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      this.canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
     });
-    canvas.add(rect);
-  }
+    this.inicia();
+  },
+  methods: {
+
+
+
+    inicia(image){
+      this.canvas.setBackgroundColor({
+        source: 'http://fabricjs.com/assets/escheresque_ste.png',
+        repeat: 'repeat',
+        offsetX: 600,
+        offsetY: 600
+      }, this.canvas.renderAll.bind( this.canvas));
+    }
+  },
 };
 </script>
